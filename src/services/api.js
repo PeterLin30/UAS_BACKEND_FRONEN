1bbfc840cb949a1a200049941c030a5630ab333e
PeterLin30 <peterlin.030@gmail.com>
2026-06-19 22:28:24 +0700

    tambahan

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://uas-backend-beken.vercel.app/api'
+    baseURL: 'https://uas-backend-beken.vercel.app'
 });
 
 API.interceptors.request.use((req) => {
