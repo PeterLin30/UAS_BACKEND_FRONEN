41312ba6a51e528dd96b9181946b46e3314a1e24
PeterLin30 <peterlin.030@gmail.com>
2026-06-23 22:28:05 +0700

    revisi

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://uas-backend-beken.vercel.app/api' 
+    baseURL: 'https://uas-backend-beken.vercel.app/api', 
 });
 
-API.interceptors.request.use((req) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        req.headers.Authorization = `Bearer ${token}`;
+API.interceptors.request.use(
+    (req) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            req.headers.Authorization = `Bearer ${token}`;
+        }
+        return req;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return req;
-});
+);
 
 export default API;
